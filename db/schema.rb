fb4d4ece81807bc17d27ddf722bdc9546547b1da
mioko.suzuki <blaaack112@gmail.com>
2020-10-08 09:57:10 +0000

    add git-tasklist

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2020_10_07_133657) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "status"
+    t.string "status"
   end
 
 end
